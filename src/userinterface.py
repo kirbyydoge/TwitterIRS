8e6d9a3d2a8336e651a428c3474f9678446598a2
kirbyydoge <aqwoguz@gmail.com>
2021-08-13 10:01:05 +0300

    Add search page to user interface.
    You can now query based on scoring model and view top 10 tweets. (May make # of tweets dynamic later).

--- src/userinterface.py
+++ src/userinterface.py
@@ -1,10 +1,22 @@
 from logging import debug
 from flask import Flask, render_template, request, json, jsonify
 from utils import scoreutils
+from wrappers.database import Database
+from wrappers.invertedindex import InvertedIndex
 import elasticbaseline
+import json
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "9fc5e33cf23b805a512fa86d3ez75b7c"
+
+DATABASE_PATH = "filedumps/initialdb.filedump"
+INDEX_PATH = "indexes/initialidx.index"
+
+db = Database(DATABASE_PATH)
+index = InvertedIndex(INDEX_PATH)
+
+db.load(DATABASE_PATH)
+index.load(INDEX_PATH)
 
 @app.route("/")
 @app.route("/home")
@@ -14,16 +26,17 @@ def home():
 @app.route("/search")
 def search():
 	return render_template("search.html")
-	
+
 @app.route("/search/query", methods=["GET", "POST"])
 def search_query():
 	data = request.get_json()
+	print(data)
 	try:
 		query = data["query"]
 		method = data["method"]
-		if method == "UNIGRAM":
-			result = scoreutils.score_unigram(query)
-		elif method == "ELASTICBM25":
+		if method == "unigram":
+			result = scoreutils.score_unigram(query, index, lamb=0.8)
+		elif method == "elasticbm25":
 			result = elasticbaseline.text_to_search(query)
 		if result:
 			result = result[:10]
@@ -32,7 +45,10 @@ def search_query():
 	except Exception as e:
 		print(e)
 		return jsonify({"success":False})
-	return jsonify({"success":True, "tweets": jsonify(result)})
+	tweetlist = []
+	for tweetid in result:
+		tweetlist.append(json.dumps(db.get(tweetid)))
+	return jsonify({"success":True, "tweets":tweetlist})
 
 if __name__ == "__main__":
 	app.run(debug=True)
